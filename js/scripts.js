9ed6dc37f66e4c152b67c45302051026a9dfec51
xack83 <xack83@gmail.com>
2015-09-05 10:44:16 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	//$("h1").addClass("text-primary");
+    $('.portfolio-item').mouseenter(function() {
+		$(this).find(':nth-child(2)').fadeIn(300);
+	});
+	$('.portfolio-item').mouseleave(function() {
+		$(this).find(':nth-child(2)').fadeOut(300);
+	});
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
+
